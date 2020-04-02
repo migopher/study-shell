cc472ed372d5eb55bb3075da8126abeff01c2e9d
xuzili <xuzili@ihuizong.cn>
2020-04-02 09:54:14 +0800

    更新文件

--- 8.sh
+++ 8.sh
@@ -7,13 +7,14 @@
 # echo "参数1：$1";
 hello="hello world"
 echo $hello
-# echo "${hello}"
+echo "${hello}"
 
-# for i in "$*"; do
-#   echo $i
-#   echo "-----"
-# done 
-
-# for j in "$@";do
-#     echo $j
-# done
+for i in "$*"; do
+  echo $i
+  echo "---1---"
+done 
+echo "-----"
+for j in "$@";do
+    echo $j
+    echo "---2---"
+done
