d4533d36f8878c5a531b8382cf61f640f9b0c5e3
xuzili <xuzili@ihuizong.cn>
2020-04-07 09:37:59 +0800

    no message

--- 11.sh
+++ 11.sh
@@ -10,6 +10,6 @@ printf "%-5s %-10s %-4s\n" 3 java 95
 
 
 echo -e "1\t2\t3"
-echo -e "\e[1;31m  This is red text \e[0m"
+echo -e "\e[33;31m  This is red text \e[0m"
 
-echo -e "\033[2;31m golang \033[0m"
+echo -e "\033[33;31m golang \033[0m"
